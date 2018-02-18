72b819bce1090736ca57e8f05a40d26de514b535
frmdstryr <frmdstryr@gmail.com>
2018-02-18 18:55:46 -0500

    Remove version from name

--- ios-openssl/build.sh
+++ ios-openssl/build.sh
@@ -32,17 +32,22 @@ do
 
     # Configure for iphoneos
     perl Configure $TARGET -shared --prefix=@rpath
+    # TODO: Use --cross-compile-prefix=$BUILD_TOOLS
 
     # Clean
     make clean
+
+    FIND='SHLIB_EXT=.\$(SHLIB_MAJOR).\$(SHLIB_MINOR).dylib'
+    sed -ie s!$FIND!SHLIB_EXT=.dylib!g Makefile
+    #sed -ie "s!LIBRPATH=\'\$(INSTALLTOP)/\$(LIBDIR)!LIBRPATH=@rpath!g" Makefile
+
 
     # Build
     make -j$CPU_COUNT build_libs LIBDIR=.
 
     # Copy to install dir
     mkdir $ARCH
-    mv libcrypto*.dylib $ARCH/
-    mv libssl*.dylib $ARCH/
+    cp -RL lib*.dylib $ARCH/
 
 done
 
@@ -52,12 +57,12 @@ mkdir $PREFIX/iphonesimulator
 mkdir $PREFIX/iphonesimulator/lib
 
 # Merge iphoneos libs
-lipo -create armv7/libssl.1.0.0.dylib \
-             arm64/libssl.1.0.0.dylib \
+lipo -create armv7/libssl.dylib \
+             arm64/libssl.dylib \
              -output $PREFIX/iphoneos/lib/libssl.dylib
 
-lipo -create armv7/libcrypto.1.0.0.dylib \
-             arm64/libcrypto.1.0.0.dylib \
+lipo -create armv7/libcrypto.dylib \
+             arm64/libcrypto.dylib \
              -output $PREFIX/iphoneos/lib/libcrypto.dylib
 cp -RL include $PREFIX/iphoneos
 
