47835f3768bb0dbe635750fba953c8635a713287
frmdstryr <frmdstryr@gmail.com>
2018-02-24 00:16:10 -0500

    Add sqlite

--- android-sqlite/build.sh
+++ android-sqlite/build.sh
@@ -3,9 +3,9 @@
 # Copyright (c) 2018, Jairus Martin.
 # Distributed under the terms of the MIT License.
 # The full license is in the file LICENSE, distributed with this software.
-# Created on Feb 10, 2018
+# Created on Feb 23, 2018
 # ==================================================================================================
-export ARCHS=("x86_64 x86 arm arm64")
+export ARCHS=("arm arm64 x86_64 x86")
 export NDK="$HOME/Android/Sdk/ndk-bundle"
 
 for ARCH in $ARCHS
@@ -14,45 +14,36 @@ do
     if [ "$ARCH" == "arm" ]; then
         export TARGET_HOST="arm-linux-androideabi"
         export TARGET_ABI="armeabi-v7a"
-        export LIBDIR="lib"
     elif [ "$ARCH" == "arm64" ]; then
         export TARGET_HOST="aarch64-linux-android"
         export TARGET_ABI="arm64-v8a"
-        export LIBDIR="lib64"
     elif [ "$ARCH" == "x86" ]; then
         export TARGET_HOST="i686-linux-android"
         export TARGET_ABI="x86"
-        export LIBDIR="lib"
     elif [ "$ARCH" == "x86_64" ]; then
         export TARGET_HOST="x86_64-linux-android"
         export TARGET_ABI="x86_64"
-        export LIBDIR="lib64"
     fi
 
     export ANDROID_TOOLCHAIN="$NDK/standalone/$ARCH"
     export APP_ROOT="$PREFIX/android/$ARCH"
     export PATH="$PATH:$ANDROID_TOOLCHAIN/bin"
     export AR="$TARGET_HOST-ar"
-    export CC="$TARGET_HOST-gcc"
-    export CXX="$TARGET_HOST-g++"
+    export CC="$TARGET_HOST-clang"
     export LD="$TARGET_HOST-ld"
+    export STRIP="$TARGET_HOST-strip"
+    export CFLAGS="-O3"
 
-    ./autogen.sh
     ./configure --host=$TARGET_HOST --prefix=$SRC_DIR/dist/$ARCH
-
-    # Clean
     make clean
-
-    # Build
     make -j$CPU_COUNT
     make install
 
-    # Copy to install dir
     mkdir -p $PREFIX/android/$ARCH/lib
-    mkdir -p $PREFIX/android/$ARCH/include
-    cp -RL dist/$ARCH/$LIBDIR/libffi.so $PREFIX/android/$ARCH/lib
-    cp -RL dist/$ARCH/lib/libffi-3.2.1/include/*.h $PREFIX/android/$ARCH/include
+    cp -RL dist/$ARCH/lib/libsqlite3.so $PREFIX/android/$ARCH/lib
+    cp -RL dist/$ARCH/include $PREFIX/android/$ARCH/
+
+    # Strip symbols
+    ${STRIP} --strip-unneeded $PREFIX/android/$ARCH/lib/libsqlite3.so
 
 done
-
-
