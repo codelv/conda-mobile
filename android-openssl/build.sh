b92ff0206f36ee6a78a4a68eb3aa10b96676f672
frmdstryr <frmdstryr@gmail.com>
2018-02-23 23:51:18 -0500

    Add openssl and minor cleanup

--- android-openssl/build.sh
+++ android-openssl/build.sh
@@ -3,56 +3,52 @@
 # Copyright (c) 2018, Jairus Martin.
 # Distributed under the terms of the MIT License.
 # The full license is in the file LICENSE, distributed with this software.
-# Created on Feb 10, 2018
+# Created on Feb 23, 2018
 # ==================================================================================================
-export ARCHS=("x86_64 x86 arm arm64")
+export ARCHS=("arm arm64 x86_64 x86")
 export NDK="$HOME/Android/Sdk/ndk-bundle"
 
 for ARCH in $ARCHS
 do
-    export ANDROID_TOOLCHAIN="$NDK/standalone/$ARCH"
-
+    CCOMPILER="clang"
     if [ "$ARCH" == "arm" ]; then
         export TARGET_HOST="arm-linux-androideabi"
-        export TARGET_ABI="armeabi-v7a"
-        export LIBDIR="lib"
+        export TARGET="android-armv7"
+        CCOMPILER="gcc" # android-armv7 doesn't seem to work with clang
     elif [ "$ARCH" == "arm64" ]; then
         export TARGET_HOST="aarch64-linux-android"
-        export TARGET_ABI="arm64-v8a"
-        export LIBDIR="lib64"
+        export TARGET="linux-aarch64"
     elif [ "$ARCH" == "x86" ]; then
         export TARGET_HOST="i686-linux-android"
-        export TARGET_ABI="x86"
-        export LIBDIR="lib"
+        export TARGET="android-x86"
+        CCOMPILER="gcc" # android-x86 doesn't seem to work with clang
     elif [ "$ARCH" == "x86_64" ]; then
         export TARGET_HOST="x86_64-linux-android"
-        export TARGET_ABI="x86_64"
-        export LIBDIR="lib64"
+        export TARGET="linux-x86_64-clang"
     fi
 
+    export ANDROID_TOOLCHAIN="$NDK/standalone/$ARCH"
     export APP_ROOT="$PREFIX/android/$ARCH"
     export PATH="$PATH:$ANDROID_TOOLCHAIN/bin"
     export AR="$TARGET_HOST-ar"
-    export CC="$TARGET_HOST-gcc"
-    export CXX="$TARGET_HOST-g++"
+    export CC="$TARGET_HOST-$CCOMPILER"
     export LD="$TARGET_HOST-ld"
 
-    ./autogen.sh
-    ./configure --host=$TARGET_HOST --prefix=$SRC_DIR/dist/$ARCH
+    # Configure for iphoneos
+    perl Configure $TARGET -shared
 
     # Clean
     make clean
 
+    FIND='SHLIB_EXT=.\$(SHLIB_MAJOR).\$(SHLIB_MINOR).so'
+    sed -ie s!$FIND!SHLIB_EXT=.so!g Makefile
+
     # Build
-    make -j$CPU_COUNT
-    make install
+    make -j$CPU_COUNT build_libs LIBDIR=.
 
     # Copy to install dir
     mkdir -p $PREFIX/android/$ARCH/lib
-    mkdir -p $PREFIX/android/$ARCH/include
-    cp -RL dist/$ARCH/$LIBDIR/libffi.so $PREFIX/android/$ARCH/lib
-    cp -RL dist/$ARCH/lib/libffi-3.2.1/include/*.h $PREFIX/android/$ARCH/include
+    cp -RL lib*.so $PREFIX/android/$ARCH/lib
+    cp -RL include $PREFIX/android/$ARCH/
 
 done
-
-
