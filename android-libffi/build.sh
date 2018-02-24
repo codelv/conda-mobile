c7e44ffcdf9b552ae692a331c5483ea634214e3b
frmdstryr <frmdstryr@gmail.com>
2018-02-23 23:12:04 -0500

    Add libffi for android

---
+++ android-libffi/build.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# ==================================================================================================
+# Copyright (c) 2018, Jairus Martin.
+# Distributed under the terms of the MIT License.
+# The full license is in the file LICENSE, distributed with this software.
+# Created on Feb 10, 2018
+# ==================================================================================================
+export PYTHON_FOR_BUILD="$(which python2)"
+export ARCHS=("x86_64 x86 arm arm64")
+export NDK="$HOME/Android/Sdk/ndk-bundle"
+
+for ARCH in $ARCHS
+do
+    export ANDROID_TOOLCHAIN="$NDK/standalone/$ARCH"
+
+    if [ "$ARCH" == "arm" ]; then
+        export TARGET_HOST="arm-linux-androideabi"
+        export TARGET_ABI="armeabi-v7a"
+        export LIBDIR="lib"
+    elif [ "$ARCH" == "arm64" ]; then
+        export TARGET_HOST="aarch64-linux-android"
+        export TARGET_ABI="arm64-v8a"
+        export LIBDIR="lib64"
+    elif [ "$ARCH" == "x86" ]; then
+        export TARGET_HOST="i686-linux-android"
+        export TARGET_ABI="x86"
+        export LIBDIR="lib"
+    elif [ "$ARCH" == "x86_64" ]; then
+        export TARGET_HOST="x86_64-linux-android"
+        export TARGET_ABI="x86_64"
+        export LIBDIR="lib64"
+    fi
+
+    export APP_ROOT="$PREFIX/android/$ARCH"
+    export PATH="$PATH:$ANDROID_TOOLCHAIN/bin"
+    export AR="$TARGET_HOST-ar"
+    export CC="$TARGET_HOST-gcc"
+    export CXX="$TARGET_HOST-g++"
+    export LD="$TARGET_HOST-ld"
+
+    ./autogen.sh
+    ./configure --host=$TARGET_HOST --prefix=$SRC_DIR/dist/$ARCH
+
+    # Clean
+    make clean
+
+    # Build
+    make -j$CPU_COUNT
+    make install
+
+    # Copy to install dir
+    mkdir -p $PREFIX/android/$ARCH/lib
+    mkdir -p $PREFIX/android/$ARCH/include
+    cp -RL dist/$ARCH/$LIBDIR/libffi.so $PREFIX/android/$ARCH/lib
+    cp -RL dist/$ARCH/lib/libffi-3.2.1/include/*.h $PREFIX/android/$ARCH/include
+
+done
+
+
