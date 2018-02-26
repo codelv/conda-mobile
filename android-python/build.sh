f973c2a8ae9f70820e8ebd3aa6f5f83883f04883
frmdstryr <frmdstryr@gmail.com>
2018-02-26 09:03:53 -0500

    It all works as shared libs now

--- android-python/build.sh
+++ android-python/build.sh
@@ -6,13 +6,15 @@
 # Created on Feb 23, 2018
 # ==================================================================================================
 export PYTHON_FOR_BUILD="$(which python2)"
-export ARCHS=("x86_64 x86 arm arm64")
+export ARCHS=("x86 x86_64 arm arm64")
 export NDK="$HOME/Android/Sdk/ndk-bundle"
 
 # Patch
-patch -t -d $SRC_DIR -p1 -i $RECIPE_DIR/patches/xcompile.patch
 patch -t -d $SRC_DIR -p1 -i $RECIPE_DIR/patches/setup.patch
 patch -t -d $SRC_DIR -p1 -i $RECIPE_DIR/patches/sqlite.patch
+
+# Change insoname
+sed -ie 's!INSTSONAME="$LDLIBRARY".$SOVERSION!!g' configure
 
 # Disable LFS
 sed -ie "s!use_lfs=yes!use_lfs=no!g" configure
@@ -26,34 +28,47 @@ do
 #                                            --stl=libc++ \
 #                                            --install-dir=$NDK/standalone/$ARCH
     export ANDROID_TOOLCHAIN="$NDK/standalone/$ARCH"
-    LIBDIR="lib"
+    EXTLIBDIR="lib"
+    EXTBUILDDIR="linux2-$ARCH"
     if [ "$ARCH" == "arm" ]; then
         export TARGET_HOST="arm-linux-androideabi"
         export TARGET_ABI="armeabi-v7a"
     elif [ "$ARCH" == "arm64" ]; then
         export TARGET_HOST="aarch64-linux-android"
         export TARGET_ABI="arm64-v8a"
+        EXTBUILDDIR="linux2-aarch64"
     elif [ "$ARCH" == "x86" ]; then
         export TARGET_HOST="i686-linux-android"
         export TARGET_ABI="x86"
+        EXTBUILDDIR="linux2-i686"
     elif [ "$ARCH" == "x86_64" ]; then
         export TARGET_HOST="x86_64-linux-android"
         export TARGET_ABI="x86_64"
-        LIBDIR="lib64"
+        EXTLIBDIR="lib64"
     fi
 
     export APP_ROOT="$PREFIX/android/$ARCH"
     export PATH="$PATH:$ANDROID_TOOLCHAIN/bin"
     export AR="$TARGET_HOST-ar"
-    export RC="$TARGET_HOST-rc"
+    export AS="$TARGET_HOST-clang"
     export CC="$TARGET_HOST-clang"
     export CXX="$TARGET_HOST-clang++"
     export LD="$TARGET_HOST-ld"
     export RANLIB="$TARGET_HOST-ranlib"
-    export STRIP="$TARGET_HOST-strip"
+
+    export LDSHARED="$CC -shared"
+    #export LIBFFI_INCLUDEDIR="$APP_ROOT/include"
+
+    export CROSS_COMPILE="$ARCH"
+    export CROSS_COMPILE_TARGET='yes'
 
     export CFLAGS="-O3 -I$APP_ROOT/include"
-    export LDFLAGS="-L$APP_ROOT/lib"
+    export LDFLAGS="-L$APP_ROOT/lib -L$ANDROID_TOOLCHAIN/sysroot/usr/$EXTLIBDIR" # -lffi -lssl -lcrypto"
+
+    # Flags for building extensions these are patched in
+    #export ANDROID_LDFLAGS="$LDFLAGS -L$SRC_DIR -lpython2.7"
+    #export ANDROID_LDSHARED="$CC -shared $LDFLAGS"
+    #export ANDROID_CFLAGS="$CFLAGS"
 
     # The magic cross compile flag (tells it to not try to load them after building)
     export _PYTHON_HOST_PLATFORM="$TARGET_HOST"
@@ -63,12 +78,18 @@ do
     cp $RECIPE_DIR/Setup.local $SRC_DIR/Modules/
     sed -ie s!SSL=/usr/local/ssl!SSL=$APP_ROOT/$SDK/include/!g $SRC_DIR/Modules/Setup.local
 
+    # Install our own setup
+    #cp -f $RECIPE_DIR/setup.py $SRC_DIR/
+
+    export
+
     ./configure ac_cv_file__dev_ptmx=no \
                 ac_cv_file__dev_ptc=no \
                 ac_cv_have_long_long_format=yes \
                 --host=$TARGET_HOST \
                 --build=$BUILD \
-                --without-pymalloc \
+                --with-threads \
+                --with-system-ffi \
                 --disable-toolbox-glue \
                 --enable-ipv6 \
                 --enable-shared \
@@ -88,11 +109,14 @@ do
     # pwdmodule
     sed -ie 's!#define HAVE_GETPWENT 1!/* #undef HAVE_GETPWENT */!g' pyconfig.h
 
+    # Build libpython
+    make -j$CPU_COUNT libpython2.7.so
 
-    # Build and install
-    make -j$CPU_COUNT CROSS_COMPILE_TARGET=yes
-    make -C $SRC_DIR install CROSS_COMPILE_TARGET=yes \
-                    prefix=$SRC_DIR/dist/$ARCH
+    # Now build the extensions and be sure to explicitly link python
+    # The new setup.py build has all kinds of errors so use the old school way
+    # To anyone that wants to try with setup.py have fun :)
+    make -j$CPU_COUNT oldsharedmods LDFLAGS="$LDFLAGS -L. -lpython2.7"
+    make -C $SRC_DIR install prefix=$SRC_DIR/dist/$ARCH
 
     # Remove unused stuff
     rm -Rf dist/$ARCH/lib/python2.7/test
@@ -102,11 +126,12 @@ do
     rm -Rf dist/$ARCH/lib/python2.7/lib-*
     rm -Rf dist/$ARCH/lib/python2.7/config
 
-
     mkdir -p $PREFIX/android/$ARCH/lib
     mkdir -p $PREFIX/android/$ARCH/python
 
     # Prefix with lib., remove module from name, and copy extensions
+    #cd build/lib.$EXTBUILDDIR-2.7; rename 's/^/lib./' *.so; cd $SRC_DIR
+    #cp -RL build/lib.$EXTBUILDDIR-2.7/*.so $PREFIX/android/$ARCH/lib/
     cd Modules; rename 's/^/lib./' *.so; rename 's/module//' *.so; cd ..
     find Modules -type f -name "*.so" -exec cp {} "$PREFIX/android/$ARCH/lib/" \;
 
@@ -116,11 +141,11 @@ do
     cp -RL dist/$ARCH/lib/python2.7/* $PREFIX/android/$ARCH/python
 
     # Include c++_shared.so
-    cp -RL $ANDROID_TOOLCHAIN/$TARGET_HOST/$LIBDIR/libc++_shared.so $PREFIX/android/$ARCH/lib
+    cp -RL $ANDROID_TOOLCHAIN/$TARGET_HOST/$EXTLIBDIR/libc++_shared.so $PREFIX/android/$ARCH/lib
 
     # Strip libs of debug symbol
     chmod 775 $PREFIX/android/$ARCH/lib/libpython2.7.so
     #find $PREFIX/android/$ARCH/lib -name "*.so" \
     #     -exec ${STRIP} --strip-unneeded {} \;
-
+    #exit 1
 done
