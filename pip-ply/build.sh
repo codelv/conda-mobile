f9b47cabeb551d6c84d6822f52186c3ce64dcba2
frmdstryr <frmdstryr@gmail.com>
2018-03-16 12:31:30 -0400

    Set CC and CXX explicitly so builds work with regular conda build

--- pip-ply/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export TARGETS=("iphoneos iphonesimulator android/arm android/arm64 android/x86 android/x86_64")
-
-# Install in each target
-for TARGET in $TARGETS
-do
-    pip install ply==$PKG_VERSION --target=$PREFIX/$TARGET/python/site-packages
-done
