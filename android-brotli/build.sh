77a3ca87741c11ee86a0560c26839bd3ab229583
frm dstryr <frmdstryr@protonmail.com>
2023-04-08 14:28:24 -0400

    Update recipes

--- android-brotli/build.sh
+++ android-brotli/build.sh
@@ -1,38 +1,35 @@
 #!/usr/bin/env bash
 # ==================================================================================================
-# Copyright (c) 2018, Jairus Martin.
+# Copyright (c) 2023, Jairus Martin.
 # Distributed under the terms of the GPL v3 License.
 # The full license is in the file LICENSE, distributed with this software.
-# Created on Apr 30, 2018
+# Created on Feb 23, 2018
 # ==================================================================================================
 source $BUILD_PREFIX/android/activate-ndk.sh
 
-
 for ARCH in $ARCHS
 do
-
     # Setup compiler for arch and target_api
     activate-ndk-clang $ARCH
 
-    #./autogen.sh
-    ./configure --host=$TARGET_HOST --prefix=$SRC_DIR/dist/$ARCH --enable-static=no \
-                --without-freetype --without-glib --without-icu --without-cairo
-
-    # Clean
-    make clean
-
-    # Build
-    make -j$CPU_COUNT
-    make install
+    rm -rf build
+    mkdir build
+    cd build
+        cmake ../ -DCMAKE_INSTALL_PREFIX=$SRC_DIR/dist/$ARCH -DBROTLI_DISABLE_TESTS=1
+        # Build
+        make -j$CPU_COUNT
+        make install
+    cd ../
 
     # Copy to install dir
     mkdir -p $PREFIX/android/$ARCH/lib
     mkdir -p $PREFIX/android/$ARCH/include
-    cp -RL dist/$ARCH/lib/libharfbuzz.so $PREFIX/android/$ARCH/lib
-    cp -RL dist/$ARCH/lib/libharfbuzz-subset.so $PREFIX/android/$ARCH/lib
+    cp -RL dist/$ARCH/lib/libbrotlidec.so $PREFIX/android/$ARCH/lib
+    cp -RL dist/$ARCH/lib/libbrotlienc.so $PREFIX/android/$ARCH/lib
+    cp -RL dist/$ARCH/lib/libbrotlicommon.so $PREFIX/android/$ARCH/lib
     cp -RL dist/$ARCH/include/* $PREFIX/android/$ARCH/include
     validate-lib-arch $PREFIX/android/$ARCH/lib/*.so
-
 done
+
 
 
